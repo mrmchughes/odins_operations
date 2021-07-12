d005699355b7eb2c1cff4fba435dced7f8951093
Raymond Wolt <rjwolt@gmail.com>
2021-07-11 19:45:41 -0500

    Add gameboard and title card

---
+++ scripts/gameBoard.js
@@ -0,0 +1,31 @@
+let gameBoard = (() => {
+  //Create a grid square element for an answer object
+  const gridSquare = (answer) => {
+    let squareEl = document.createElement('div');
+    squareEl.classList.add('grid-square');
+    //Create an element to show the answer and append it to the square
+    let answerEl = document.createElement('p');
+    answerEl.classList.add('grid-answer');
+    answerEl.innerText = answer;
+    squareEl.appendChild(answerEl);
+    return squareEl;
+  };
+
+  //Create a board to hold all the grid squares
+  const drawBoard = (answerArray) => {
+    let board = document.createElement('div');
+    board.classList.add('game-board');
+    //Iterate over the answer array, create a grid square for each answer and append it to the board
+    answerArray.forEach((item, index) => {
+      let square = gridSquare(item.answer);
+      square.dataset.index = index;
+      square.id = 'grid-' + index;
+      board.appendChild(square);
+    });
+    //Append the board to the body
+    document.body.appendChild(board);
+  };
+  return { drawBoard };
+})();
+
+export { gameBoard };
