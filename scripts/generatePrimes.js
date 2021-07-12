0b5d73854ba891fee8fa7cb031e0bbc6269037ad
Raymond Wolt <rjwolt@gmail.com>
2021-07-12 17:13:03 -0500

    Add function to generate an array of prime numbers

---
+++ scripts/generatePrimes.js
@@ -0,0 +1,46 @@
+let generatePrimes = (lowerBound, upperBound) => {
+  //Function to check if a number is prime
+  const isPrime = (number) => {
+    if (number === 1) {
+      return false;
+    } else {
+      let i = number - 1;
+      while (i > 1) {
+        if (number % i === 0) {
+          return false;
+        }
+        i--;
+      }
+      return true;
+    }
+  };
+  //Create an array of objects with prime numbers
+  let primesArray = [];
+  let answer;
+  let i = 0;
+  while (i < 30) {
+    let diceRoll = Math.random();
+    //40 percent of the time generate a prime number
+    if (diceRoll < 0.4) {
+      do {
+        answer = Math.floor(
+          Math.random() * (upperBound - lowerBound + 1) + lowerBound
+        );
+      } while (!isPrime(answer));
+      //Else return a non-prime number within the number range
+    } else {
+      do {
+        answer = Math.floor(
+          Math.random() * (upperBound - lowerBound + 1) + lowerBound
+        );
+      } while (isPrime(answer));
+    }
+    //Check if the answer is prime
+    isCorrect = isPrime(answer);
+    //Store the answer in an object
+    let numObject = { answer, isCorrect };
+    primesArray.push(numObject);
+    i++;
+  }
+  return { primesArray };
+};
