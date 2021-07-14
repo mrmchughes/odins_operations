6daf50f26e6d39f90548b71604dc7b47941373ba
Raymond Wolt <rjwolt@gmail.com>
2021-07-13 20:05:24 -0500

    Add script to draw the character to the game board

---
+++ scripts/character.js
@@ -0,0 +1,61 @@
+let character = (() => {
+  let createMuncher = () => {
+    let muncher = document.createElement('div');
+    muncher.classList.add('character');
+    muncher.style.position = 'absolute';
+    muncher.style.left = '0px';
+    muncher.style.top = '0px';
+    muncher.style.backgroundImage = "url('../images/goldenmuncher_100.gif')";
+    return muncher;
+  };
+
+  document.addEventListener('keydown', function (e) {
+    let key = e.key;
+    switch (key) {
+      case 'ArrowRight': //Right arrow key
+        moveRight();
+        console.log(key);
+        break;
+      case 'ArrowLeft': //Left arrow key
+        moveLeft();
+        break;
+      case 'ArrowUp': //Up arrow key
+        moveUp();
+        break;
+      case 'ArrowDown': //Down arrow key
+        moveDown();
+        break;
+    }
+  });
+
+  function moveRight() {
+    let character = document.querySelector('.character');
+    if (parseInt(character.style.left) < 500) {
+      character.style.left = parseInt(character.style.left) + 100 + 'px';
+    }
+  }
+
+  function moveLeft() {
+    let character = document.querySelector('.character');
+    if (parseInt(character.style.left) > 0) {
+      character.style.left = parseInt(character.style.left) - 100 + 'px';
+    }
+  }
+
+  function moveUp() {
+    let character = document.querySelector('.character');
+    if (parseInt(character.style.top) > 0) {
+      character.style.top = parseInt(character.style.top) - 100 + 'px';
+    }
+  }
+
+  function moveDown() {
+    let character = document.querySelector('.character');
+    if (parseInt(character.style.top) < 400) {
+      character.style.top = parseInt(character.style.top) + 100 + 'px';
+    }
+  }
+  return { createMuncher, moveRight };
+})();
+//Comment here
+export default character;
