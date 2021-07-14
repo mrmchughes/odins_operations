9765a5a419e2bd4ef99aeffd9a78ad5a5ef6c12d
Raymond Wolt <rjwolt@gmail.com>
2021-07-13 20:12:55 -0500

    Modify main.js to test character

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { generateMultiples } from './scripts/generateMultiples.js';
 import { generateFactors } from './scripts/generateFactors.js';
 import { gameBoard } from './scripts/gameBoard.js';
+import character from './scripts/character.js';
 
 //Create a new factors object
 let factorsObj = generateFactors(2, 25);
@@ -10,4 +11,5 @@ let titleCard = document.createElement('h2');
 titleCard.innerText = 'Factors of ' + factorsObj.base;
 document.body.appendChild(titleCard);
 gameBoard.drawBoard(factorsObj.factorsArray);
-
+let munch = character.createMuncher();
+document.querySelector('.game-board').appendChild(munch);
