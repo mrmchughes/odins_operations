ee7883597b35c43976e26d5300995b3ed2da530b
Michael Hughes <mrmchughes27@gmail.com>
2021-07-15 11:00:53 -0700

    Add untracked files

---
+++ scripts/game_over.js
@@ -0,0 +1,10 @@
+//when player selects wrong answer//
+//display message saying, 'sorry, that is an incorrect answer//
+//then reset the page or character?//
+
+//when game over is triggered, display message (alert?) saying 'thanks for playing!'//
+//have button appear after message to reset the game when pressed//
+
+const gameOver = () => {
+    alert('Sorry, but that is a game over.', 'If you would like to play again, please press the reset button.', 'Otherwise, please close the page.');
+};
