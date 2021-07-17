41ebdc9aedf4765615d11b83fffcb73451216ba0
LorriAnn <LorriAnn7@att.net>
2021-07-16 20:57:47 -0400

    toggles practice area buttons

--- scripts/startMenu.js
+++ scripts/startMenu.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-unused-vars */
 import { gameController } from './gameController.js';
 import { cssLoader } from './cssLoader.js';
 cssLoader.load('styles.css');
+
 const startMenu = () => {
+
+  // creates header text
+  
   const drawHeader = (() => {
     const header = document.querySelector('#header');
     const titleHeader = document.createElement('h1');
@@ -19,8 +24,12 @@ const startMenu = () => {
     console.log('page direct');
   }
 
+  // creates container with play options
+
   const startContainer = (() => {
     const container = document.querySelector('#container');
+
+    // creates input for players name
 
     const nameInputTitle = document.createElement('h3');
     nameInputTitle.innerHTML = 'User Name';
@@ -32,6 +41,8 @@ const startMenu = () => {
     userNameInput.style.backgroundColor = 'gray';
     container.appendChild(userNameInput);
 
+    // creates practice area button container
+
     const practiceAreaTitle = document.createElement('h3');
     practiceAreaTitle.innerHTML = 'Practice Area';
     practiceAreaTitle.id = 'practice-title';
@@ -41,17 +52,25 @@ const startMenu = () => {
     practiceContainer.className = 'practice-container';
     container.appendChild(practiceContainer);
 
+    // defines default options
+
     let practiceArea = 'multiples';
     let difficulty = 'intermediate';
+
+    // creates buttons to select math area to practice
 
     const selectMultiples = document.createElement('button');
     selectMultiples.innerHTML = 'Multiples';
     selectMultiples.className = 'practice-area';
     selectMultiples.id = 'multiples';
+    selectMultiples.style.backgroundColor = 'hotpink'; 
     practiceContainer.appendChild(selectMultiples);
 
     const multiplesButton = document.getElementById('multiples');
     multiplesButton.addEventListener('click', function () {
+      selectMultiples.style.backgroundColor = 'hotpink'; 
+      selectFactors.style.backgroundColor = 'whitesmoke';
+      selectPrimes.style.backgroundColor = 'whitesmoke';
       practiceArea = 'multiples';
     });
 
@@ -63,6 +82,9 @@ const startMenu = () => {
 
     const factorsButton = document.getElementById('factors');
     factorsButton.addEventListener('click', function () {
+      selectMultiples.style.backgroundColor = 'whitesmoke'; 
+      selectFactors.style.backgroundColor = 'hotpink';
+      selectPrimes.style.backgroundColor = 'whitesmoke';
       practiceArea = 'factors';
     });
 
@@ -74,8 +96,13 @@ const startMenu = () => {
 
     const primesButton = document.getElementById('primes');
     primesButton.addEventListener('click', function () {
+      selectMultiples.style.backgroundColor = 'whitesmoke'; 
+      selectFactors.style.backgroundColor = 'whitesmoke';
+      selectPrimes.style.backgroundColor = 'hotpink';
       practiceArea = 'primes';
     });
+
+    // TODO: add equalities, inequalities, or another option
 
     // const selectInequalities = document.createElement('button');
     // selectInequalities.innerHTML = 'Inequalities';
@@ -85,6 +112,8 @@ const startMenu = () => {
     // selectEqualities.innerHTML = 'Equalities';
     // selectEqualities.className = 'practice-area';
     // practiceContainer.appendChild(selectEqualities);
+
+    // creates button to enable the game to begin
 
     const beginContainer = document.createElement('div');
     beginContainer.className = 'begin-container';
