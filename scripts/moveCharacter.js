2ea111303ca1339fe0d53b3770aff1050deeaed3
LorriAnn <LorriAnn7@att.net>
2021-07-17 20:51:35 -0400

    changes grid square color upon selection

--- scripts/moveCharacter.js
+++ scripts/moveCharacter.js
@@ -3,6 +3,12 @@ const moveCharacter = (() => {
   const moveResponse = (position, answerObject) => {
     console.log('move character ' + position);
     console.log(answerObject);
+
+    // changes grid square color upon selection
+
+    const selectedGrid = document.getElementById('grid-' + position);
+    selectedGrid.style.backgroundColor = 'white';
+    selectedGrid.style.color = 'black';
   }
 
   return { moveResponse };
