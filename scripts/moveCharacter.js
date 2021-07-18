fd7ba56a56f71ef24c7b2f0315266fa285e91fea
LorriAnn <LorriAnn7@att.net>
2021-07-17 21:21:14 -0400

    changes grid square color depending on whether correct or incorrect choice is made

--- scripts/moveCharacter.js
+++ scripts/moveCharacter.js
@@ -1,15 +1,31 @@
 const moveCharacter = (() => {
 
   const moveResponse = (position, answerObject) => {
-    console.log('move character ' + position);
-    console.log(answerObject);
+    const answerCorrect = answerObject.numbersArray[position].isCorrect;
+    const selectedGrid = document.getElementById('grid-' + position);
 
     // changes grid square color upon selection
 
-    const selectedGrid = document.getElementById('grid-' + position);
-    selectedGrid.style.backgroundColor = 'white';
+    selectedGrid.style.backgroundColor = 'rgb(250, 155, 202)';
     selectedGrid.style.color = 'black';
-  }
+
+    // changes grid square based on whether correct or incorrect selection is made
+
+    document.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        if (answerCorrect) {
+          selectedGrid.style.backgroundColor = 'hotpink';
+          // TODO: add sound for correct choice
+        } else {
+          selectedGrid.style.backgroundColor = 'red';
+          // TODO: add sound for incorrect choice
+        }
+      }
+    });
+    
+  }  
+
+
 
   return { moveResponse };
 
