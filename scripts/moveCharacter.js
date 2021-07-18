ae614dd104c6619bcd2d13a4f48b551d556f1d0e
LorriAnn <LorriAnn7@att.net>
2021-07-17 22:28:44 -0400

    fixes grid color bug

--- scripts/moveCharacter.js
+++ scripts/moveCharacter.js
@@ -1,32 +1,34 @@
 const moveCharacter = (() => {
 
+  let answerCorrect = 'true';
+  let selectedGrid;
+
   const moveResponse = (position, answerObject) => {
 
-    let selectedGrid = document.getElementById('grid-' + position);
+    selectedGrid = document.getElementById('grid-' + position);
+    answerCorrect = answerObject.numbersArray[position].isCorrect;
 
     // changes grid square color upon selection
 
     selectedGrid.style.backgroundColor = 'rgb(250, 155, 202)';
     selectedGrid.style.color = 'black';
-
-    // changes grid square based on whether correct or incorrect selection is made
-
-    document.addEventListener('keypress', function (e) {
-      if (e.key === 'Enter') {
-        const answerCorrect = answerObject.numbersArray[position].isCorrect;
-        if (answerCorrect) {
-          selectedGrid.style.backgroundColor = 'hotpink';
-          // TODO: add sound for correct choice
-        } else {
-          selectedGrid.style.backgroundColor = 'red';
-          // TODO: add sound for incorrect choice
-        }
-      } else {
-        selectedGrid.style.backgroundColor = 'rgb(250, 155, 202)';
-      }
-    });
-    
   }  
+
+  // changes grid square based on whether correct or incorrect selection is made
+
+  document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        if (answerCorrect) {
+        selectedGrid.style.backgroundColor = 'hotpink';
+        // TODO: add sound for correct choice
+        } else {
+        selectedGrid.style.backgroundColor = 'red';
+        // TODO: add sound for incorrect choice
+        }
+    } else {
+        selectedGrid.style.backgroundColor = 'rgb(250, 155, 202)';
+    }
+  });
 
   return { moveResponse };
 
